c917008d6d6f34dbf55a82750862788e1ff0c4e3
ngautier <nclgautier@gmail.com>
2018-09-12 12:06:52 +0200

    First commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# example_functions# example_deployed_functions
---
+++ functions/r-hello/handler.R
@@ -0,0 +1,8 @@
+
+#!/usr/bin/env Rscript
+
+f <- file("stdin")
+open(f)
+line<-readLines(f, n=1, warn = FALSE)
+
+write(paste0("Hi ", line), stderr())
